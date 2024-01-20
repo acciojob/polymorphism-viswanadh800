ecba560ee216c076e29288148c59bcd999b91fee
viswanadh800 <viswanadh.800@gmail.com>
2024-01-20 09:00:18 +0530

    Done as instructed in readme

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args){
+        Product p=new Product();
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1.0,2.3));
+    }
 }
